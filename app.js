a04ca3aec408e79f4214659188e618d63eed5435
João Avila <joao.avila.rs@gmail.com>
2024-09-30 16:39:09 -0300

    api funcioando

--- app.js
+++ app.js
@@ -3,25 +3,59 @@ var helmet = require("helmet");
 var cors = require("cors");
 var venom = require("venom-bot");
 
-venom
-  .create({
-    session: 'API-ZAP', // nome da sessão
-    browserArgs: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
-    ],
-    executablePath: '/usr/bin/google-chrome-stable' // Caminho para o Chrome instalado
-  })
-  .then((client) => start(client))
-  .catch((erro) => {
-    console.log(erro);
+let serviceStatus = 'Initializing'; 
+
+
+venom.create({
+  session: 'API-ZAP', // nome da sessão
+  browserArgs: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ],
+  executablePath: '/usr/bin/google-chrome-stable', // Caminho para o Chrome instalado
+  disableSpins: true, // Desativar spins para melhorar a persistência de sessão
+  cacheEnabled: false, // Desativar cache para evitar problemas de sessão
+  useChrome: true, // Usar o Chrome ao invés do Chromium (padrão)
+  puppeteerOptions: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // Argumentos do Puppeteer
+  },
+  sessionDataPath: './session', // Caminho para armazenar os dados da sessão
+  logQR: true
+},
+(statusSession, session) => {
+  console.log('Status Session: ', statusSession);
+  console.log('Session name: ', session);
+  serviceStatus = statusSession; // Atualiza a variável com o status atual
+}
+)
+.then((client) => {
+  // Escuta mudanças de estado
+  client.onStateChange((state) => {
+    console.log('Estado da sessão:', state);
+    serviceStatus = state; // Atualiza a variável com o estado atual
+    if (state === 'CONFLICT' || state === 'UNLAUNCHED') {
+      client.useHere(); // Força a sessão a ser usada aqui
+    }
   });
+
+   // Opcional: Escuta mudanças na stream (usado para reconexões)
+   client.onStreamChange((state) => {
+    console.log('Estado da stream:', state);
+  });
+
+  start(client);
+})
+.catch((erro) => {
+  console.error('Erro ao iniciar o venom-bot:', erro);
+  serviceStatus = 'Error'; // Define o status como erro
+});
+
 
 function start(client) {
   var app = express();
@@ -30,7 +64,7 @@ function start(client) {
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"], // Permite scripts inline
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Permite scripts inline e unsafe-eval
         styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", "data:"],
         connectSrc: ["'self'", "http://localhost:4000"]
@@ -81,5 +115,9 @@ function start(client) {
       console.error('Erro ao obter os grupos:', error);
       res.status(500).json({ error: 'Erro ao obter os grupos' });
     }
+  });
+   // Endpoint para obter o status do serviço
+   app.get("/api/status", (req, res) => {
+    res.json({ status: serviceStatus });
   });
 }
